41c0862a7313ca6bc6211d4f9c65eaafb74ecb62
MexMass <ramonas.lukas@gmail.com>
2026-03-27 21:16:54 +0000

    init 2

---
+++ src/main/java/shelly_grounded/ShellyGroundedApplication.java
@@ -0,0 +1,13 @@
+package shelly_grounded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellyGroundedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellyGroundedApplication.class, args);
+	}
+
+}
