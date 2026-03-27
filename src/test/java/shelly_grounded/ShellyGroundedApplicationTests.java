36816b2d58b61966d4273df8805b05952369dc26
MexMass <ramonas.lukas@gmail.com>
2026-03-27 21:10:57 +0000

    init

---
+++ src/test/java/shelly_grounded/ShellyGroundedApplicationTests.java
@@ -0,0 +1,13 @@
+package shelly_grounded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellyGroundedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
